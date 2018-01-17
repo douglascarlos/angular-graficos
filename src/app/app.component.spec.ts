f1dd477f7f1d8e23b7b1ce57828e151662ab1491
Douglas Carlos <dnevescarlos@gmail.com>
2018-01-17 02:08:08 -0200

    criando modulo e componente do dashboard

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,17 @@
 import { TestBed, async } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        DashboardModule
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
